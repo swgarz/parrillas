9e96bf8e582455e93f3bedb4f5ba14f72507adf3
swgarz <delagarzadgc@gmail.com>
2020-12-07 16:46:16 -0600

    whatsapp icon

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Header from './header';
 import Container from './container';
 import "bootstrap/dist/css/bootstrap.min.css";
+import whats from './img/whatsapp.svg';
 
 function App() {
   return (
     <div className="">
         <Header />
         <Container />
-        
+        <Container />
+        <Container />
+        <div className="d-flex justify-content-center">
+          
+          <a  href="">
+            <img className="whatsapp" src={whats} alt=""/>
+          </a>
+        </div>
     </div>
   );
 }
