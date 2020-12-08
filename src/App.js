7009d699c0a6ef93157ad2cbe3501005bb151207
swgarz <delagarzadgc@gmail.com>
2020-12-07 20:19:30 -0600

    Datos Juan

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Container />
         <Container />
         <div className="d-flex justify-content-center">
-          <a href="https://wa.me/+525548822845?text=Me%20interesa%20el%20auto%20que%20estás%20vendiendo">
+          <a href="https://wa.me/+525560702145?text=Me%20interesa%20comprar%20una%20parrilla%20para%20moto">
             <img className="whatsapp" src={whats} alt=""/>
           </a>
         </div>
